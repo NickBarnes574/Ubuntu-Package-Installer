b6a0bbd55f3dc4fd017562e40ea4a71b4a5c3906
Nick Barnes <nick.a.barnes44@gmail.com>
2023-02-01 14:34:25 -0500

    create initial install script

---
+++ ubuntu_package_installer.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# NOTE: This script assumes you are running Ubuntu Linux 22.04
+
+# Perform initial update/upgrade
+sudo apt update && sudo apt dist-upgrade -y
+
+sudo apt install build-essential curl file git
+
+#---------------------------
+# CURL
+#---------------------------
+sudo apt-get install curl
+
+#---------------------------
+# PIP
+#---------------------------
+sudo apt install python3-pip
+
+#---------------------------
+# GIT
+#---------------------------
+
+# 1. Install Git
+sudo apt-get install git-all
+
+# 2. Configure user name
+git config --global user.name "Nick Barnes"
+
+# 3. Configure email
+git config --global user.email "nick.a.barnes44@gmail.com"
+
+#---------------------------
+# VS CODE
+#---------------------------
+
+# 1. Update the packages index and install dependencies
+sudo apt install software-properties-common apt-transport-https wget -y
+
+# 2. Import the Microsoft GPG key
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+# 3. Enable the VS Code repository
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# 4. Install VS Code
+sudo apt install code
+
+#---------------------------
+# SPOTIFY
+#---------------------------
+
+# 1. Configure the debian repository
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 2. Install Spotify
+sudo apt-get update && sudo apt-get install spotify-client
+
+#---------------------------
+# ZSH
+#---------------------------
+
+# 1. Install ZSH
+sudo apt install zsh
+
+# 2. Make it the default shell
+chsh -s $(which zsh)
+
+#--------------------------------------
+######## INSTALLATION COMPLETE ########
+#--------------------------------------
+echo 'Finished installation procedure!'
+echo 'NOTE: Please log out and log back in again to use ZSH'
